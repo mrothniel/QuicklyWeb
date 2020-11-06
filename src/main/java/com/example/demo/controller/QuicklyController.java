60c0a329aaa0181097af0ef56f1d8f82090b0074
mrothniel <mrothnielkk1@gmail.com>
2020-11-06 13:51:32 +0100

    api Rest for select apprenant and select all modules

--- src/main/java/com/example/demo/controller/QuicklyController.java
+++ src/main/java/com/example/demo/controller/QuicklyController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.*;
 import com.example.demo.entities.Coc_apprenant;
+import com.example.demo.entities.Coc_ens_app_exer;
 import com.example.demo.entities.Coc_enseignant;
+import com.example.demo.entities.Coc_module;
 import com.example.demo.service.QuicklyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -42,4 +44,11 @@ public class QuicklyController {
         return quicklyService.findApprenants(e.getId());
     }
 
+    @RequestMapping(value = "/apprenant/{idApprenant}", method = RequestMethod.GET)
+    public Coc_apprenant getApprenantFromId(@PathVariable Long idApprenant){
+        return quicklyService.selectApprenant(idApprenant);
+    }
+
+    @RequestMapping(value = "/list/apprenant/{idEnseignant}", method = RequestMethod.GET)
+    public List<Coc_module> getAllModules(){return coc_moduleRepositories.findAll();}
 }
