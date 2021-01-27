977d3a665d49a2df63edf4423a1df54c91db968e
mrothniel <mrothnielkk1@gmail.com>
2021-01-27 14:57:20 +0100

    add JwtAuthenticationController in controller package

--- src/main/java/com/example/demo/config/JwtRequestFilter.java
+++ src/main/java/com/example/demo/config/JwtRequestFilter.java
@@ -1,24 +1,23 @@
 package com.example.demo.config;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.example.demo.service.JwtUserDetailsService;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.ExpiredJwtException;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@ComponentScan
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
 	@Autowired
